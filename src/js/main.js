2afcebe0c663c89f525431673484cf345a6de9e9
Sitaram Gurjar <51367985+koliboy@users.noreply.github.com>
2024-01-04 11:58:28 -0800

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,30 @@
 Object.defineProperties(this, {
     $arriveAdd: {
-        value: function (targetSelector, callback) {  
-          /* 
-          $arriveAdd(match,callback)
-          https://developer.mozilla.org/en-US/docs/Web/API/Element/matches
-          */
-          
-           const observer = new MutationObserver(function(mutationsList) {
-    for (const mutation of mutationsList) {
-      if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
-        const addedElement = mutation.addedNodes[0];
-        if (addedElement.matches(targetSelector)) {
-          callback(addedElement);
-        }
-      }
-    }
-  });
+        value: function (targetSelector, callback) { /*call(element,MutationRecord])*/
+        
+            const observer = new MutationObserver(function(mutationsList) {
+                for (const mutation of mutationsList) {
+                    /* 
+                     $arriveAdd(match,callback)
+                     https://developer.mozilla.org/en-US/docs/Web/API/Element/matches
+                       */
+                    if (mutation.addedNodes.length > 0) {
+                        const addedElement = mutation.addedNodes[0];
+                        if (addedElement.matches(targetSelector)) {
+                            callback(addedElement,mutation);
+                        }
+                    }
+                }
+            });
 
-  const observerConfig = { childList: true, subtree: true };
-  observer.observe(document, observerConfig);
+            const observerConfig = {
+                childList: true,
+                attributes: true,
+                subtree: true,
+            };
+
+            observer.observe(document, observerConfig);
         },
         writable: false
     }
 });
-
