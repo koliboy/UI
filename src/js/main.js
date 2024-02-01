da9b192ea1a016b0bd528022598523d93f52e915
Sitaram Gurjar <51367985+koliboy@users.noreply.github.com>
2024-02-02 02:15:49 +0530

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -224,3 +224,19 @@ var HTP__$_$ =   $http.post(posth__$__$,$op__$__$).done(function(data__$_$,ht__$
     });
 
 }();
+
+
+function $d_pinchZoom() { /*mobile only*/
+    if ($getDeviceType() == "Mobile") {
+        document.addEventListener('touchmove', function (event) {
+            if (event.touches.length > 1) {
+                event.preventDefault();
+            }
+        }, { passive: false });
+        document.addEventListener('touchstart', function (event) {
+            if (event.touches.length > 1) {
+                event.preventDefault();
+            }
+        }, { passive: false });
+    }
+};
