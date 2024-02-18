b0ada1b418d12835d3d44738a05ce084abb7987e
Sitaram Gurjar <51367985+koliboy@users.noreply.github.com>
2024-02-18 23:54:48 +0530

    Create htp-load.js

---
+++ src/js/http/htp-load.js
@@ -0,0 +1,56 @@
+!function () {
+
+    function htp_call(scop = {}) {
+        $htp.call(this, scop);
+    }
+    setInterval(function () {
+        var htpload = $qsall("[htp-load]");
+        htpload.forEach(function (e) {
+            if (e.getAttribute("set-htp-load") == null) {
+                var bind = e.getAttribute("htp-load");
+                var target = e.getAttribute("htp-load-t");
+                e.setAttribute("set-htp-load", "t");
+                if (bind == "void") {
+                    htp_call.call(e)
+
+                } else if (target && target == "self") {
+                    e.addEventListener(bind, function () {
+                        if (e.getAttribute("htp-on") == null) {
+                            var lastend = e.querySelectorAll("[htp-load-end]") /*if scrolling*/
+                            var last = lastend[lastend.length - 1];
+
+                            var attechs = {}
+                            if (last) {
+                                attechs.end = last.getAttribute("htp-load-end");
+                            }
+                            htp_call.call(this, attechs);
+
+                        }
+
+                    });
+
+
+                } else if (target) {
+                    e.addEventListener(bind, function () {
+                        var def_ = $qs(target) || undefined;
+                        if (def_) {
+                            if (e.getAttribute("htp-on") == null) {
+                                htp_call.call(def_)
+                            }
+
+                        }
+
+                    });
+
+
+
+                }
+
+
+            }
+
+        })
+
+    });
+
+}();
