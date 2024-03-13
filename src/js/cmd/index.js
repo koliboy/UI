e35e6e09539e5440cac5ca84c2c7bfa306060fac
Sitaram Gurjar <51367985+koliboy@users.noreply.github.com>
2024-03-13 23:17:23 +0530

    Update index.js

--- src/js/cmd/index.js
+++ src/js/cmd/index.js
@@ -3,9 +3,11 @@
         var cmd = $qsall('[cmd-k]');
         cmd.forEach(function (c) {
             if (c.getAttribute("set-cmd") == null) {
+                c.setAttribute("set-cmd", "t")
                 var k = c.getAttribute("cmd-k", "t");
                 var conditions = c.getAttribute("cmd-if") || undefined;
-                var prevent = c.getAttribute("cdm-p") == "true" ? true : false || false;
+                var prevent = c.getAttribute("cmd-p") == "true" ? true : false;
+
                 var event_handle = c.getAttribute("cmd-event") || undefined;
                 $cmd(k, conditions, prevent, function (event) {
                     if (event_handle) {
